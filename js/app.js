55452dd01c3a10b5657a7476ff2a031d45c617b7
William del Rosario <williamdelrosario@yahoo.com>
2017-08-15 13:12:39 +0200

    Initialized Map and listed locations using KO

--- js/app.js
+++ js/app.js
@@ -1,24 +1,32 @@
 // first goal: display a list with location names using Knockout.js (add the map later)
 
 var ViewModel = function(){
-	this.name = ko.observable('Park Ave Penthouse');
-
-ko.applyBindings(new ViewModel());
-
-
-// hard coded Array of location objects
-// https://github.com/udacity/ud864/blob/master/Project_Code_5_BeingStylish.html#L150
-/*locations = [
+  var self = this;
+  self.locations=  [
           {name: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
           {name: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
           {name: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
           {name: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
           {name: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
           {name: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
-        ];
-        */
+        ]; 
+	};
+ko.applyBindings(new ViewModel());
+
+// hard coded Array of location objects
+// https://github.com/udacity/ud864/blob/master/Project_Code_5_BeingStylish.html#L150
+        
 
 // initMap function (later)
+      var map;
+      function initMap() {
+        // Constructor creates a new map - only center and zoom are required.
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 40.7413549, lng: -73.9980244},
+          zoom: 13
+        });
+      }
+
 // https://developers.google.com/maps/documentation/javascript/examples/map-simple
 
 // Location constructor similiar to the Cat constructor form the JavaScript Design Patterns course (optional)
