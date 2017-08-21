0607d4c35e44fc2a8eaeeb41c53d8547a87f6801
William del Rosario <williamdelrosario@yahoo.com>
2017-08-21 11:04:23 +0200

    Added listings button

--- js/app.js
+++ js/app.js
@@ -11,9 +11,10 @@ var locations=  [
 
 var ViewModel = function(){
   var self = this;
+      this.locations = ko.observableArray(locations);
       this.markers = ko.observableArray(locations);
-	};
-
+      this.filter = ko.observable("Bert");
+  };
 
 // hard coded Array of location objects
 // https://github.com/udacity/ud864/blob/master/Project_Code_5_BeingStylish.html#L150
@@ -27,7 +28,8 @@ var ViewModel = function(){
         // Constructor creates a new map - only center and zoom are required.
         map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: 40.7413549, lng: -73.9980244},
-          zoom: 13
+          zoom: 13,
+          mapTypeControl: false
         });
 
         var largeInfowindow = new google.maps.InfoWindow();
@@ -37,9 +39,8 @@ var ViewModel = function(){
           // Get the position from the location array.
           var position = locations[i].location;
           var title = locations[i].name;
-          // Create a marker per location, and put into markers array.
 
-          
+          // Create a marker per location, and put into markers array.
           var marker = new google.maps.Marker({
             map: map,
             position: position,
@@ -53,31 +54,48 @@ var ViewModel = function(){
           marker.addListener('click', function() {
             populateInfoWindow(this, largeInfowindow);
           });
-          bounds.extend(marker.position);
+         // Push the marker to our array of markers.
+          markers.push(marker);
+          // Create an onclick event to open an infowindow at each marker.
+          marker.addListener('click', function() {
+            populateInfoWindow(this, largeInfowindow);
+          });
         }
-        // Extend the boundaries of the map for each marker
-        map.fitBounds(bounds);
-
-        ko.applyBindings(new ViewModel());
-      };
-
+        document.getElementById('show-listings').addEventListener('click', showListings);
+        document.getElementById('hide-listings').addEventListener('click', hideListings);
+      }
     
       // This function populates the infowindow when the marker is clicked. We'll only allow
       // one infowindow which will open at the marker that is clicked, and populate based
       // on that markers position.
       function populateInfoWindow(marker, infowindow) {
         // Check to make sure the infowindow is not already opened on this marker.
-        console.log(marker)
         if (infowindow.marker != marker) {
           infowindow.marker = marker;
           infowindow.setContent('<div>' + marker.title + '</div>');
           infowindow.open(map, marker);
           // Make sure the marker property is cleared if the infowindow is closed.
-          infowindow.addListener('closeclick',function(){
-            infowindow.setMarker = null;
+          infowindow.addListener('closeclick', function() {
+            infowindow.marker = null;
           });
         }
-      } 
+      }
+      // This function will loop through the markers array and display them all.
+      function showListings() {
+        var bounds = new google.maps.LatLngBounds();
+        // Extend the boundaries of the map for each marker and display the marker
+        for (var i = 0; i < markers.length; i++) {
+          markers[i].setMap(map);
+          bounds.extend(markers[i].position);
+        }
+        map.fitBounds(bounds);
+      }
+      // This function will loop through the listings and hide them all.
+      function hideListings() {
+        for (var i = 0; i < markers.length; i++) {
+          markers[i].setMap(null);
+        }
+      }
 // https://developers.google.com/maps/documentation/javascript/examples/map-simple
 
 // Location constructor similiar to the Cat constructor form the JavaScript Design Patterns course (optional)
