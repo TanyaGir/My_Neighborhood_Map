bc2f6d9e95e79f5aa16d987ac7a4ba6e4a6fc157
William del Rosario <williamdelrosario@yahoo.com>
2017-09-10 16:48:45 +0200

    connect clickedLocation

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ var markers = [];
 var locations=  [
           {
           name: 'Park Ave Penthouse',
-          location: {lat: 40.7713024, lng: -73.9632393}
+          location: {lat: 40.7713024, lng: -73.9632393},
           },
           {
           name: 'Chelsea Loft',
@@ -19,46 +19,51 @@ var locations=  [
           },
           {
           name: 'East Village Hip Studio',
-          location: {lat: 40.7281777, lng: -73.984377}},
+          location: {lat: 40.7281777, lng: -73.984377}
+          },
           {
           name: 'TriBeCa Artsy Bachelor Pad', 
-          location: {lat: 40.7195264, lng: -74.0089934}},
+          location: {lat: 40.7195264, lng: -74.0089934}
+          },
           {
           name: 'Chinatown Homey Space', 
           location: {lat: 40.7180628, lng: -73.9961237}
         }];
 
 var Location = function(data) {
-  this.name = data.name;
+  this.name = ko.observable(data.name);
   this.visible = ko.observable(true);
+  this.marker = ko.observable(data.marker);
 };
 
 
 var ViewModel = function(){
   var self = this;
 
-     this.myLocations = ko.observableArray();
+    this.myLocations = ko.observableArray([]);
 
-      locations.forEach(function(location) {
-        self.myLocations.push(new Location(location));
+    locations.forEach(function(locationItem) {
+      self.myLocations.push(new Location(locationItem));
       });
+    this.currentlocation = ko.observable(this.myLocations() [0]);
 
-      this.markers = ko.observableArray(locations);
-      this.address = ko.observable("");
+    this.markers = ko.observableArray(locations);
+    this.address = ko.observable("");
       // http://knockoutjs.com/documentation/click-binding.html#note-1-passing-a-current-item-as-a-parameter-to-your-handler-function
-      this.doSomething = function(clickedLocation) {
+    this.doSomething = function(clickedLocation) {
         //console.log("click");
-      populateInfoWindow(clickedLocation.markers);
-
-      console.log(clickedLocation);
+        self.currentlocation(clickedLocation);
+        populateInfoWindow(clickedLocation.markers);
+        console.log(clickedLocation);
         // use location.marker to open the marker's info window
       };
-      this.search = function(value) {
+    this.search = function(value) {
     // remove all the current locations, which removes them from the view
       if (value) {
-        //viewModel.myLocations.removeAll();
+        //viewModel.myLocations.removeAll()
 
       //this.mylocations().forEach
+        
       for(var x in locations) {  
         if(locations[x].name.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
           self.myLocations()[x].visible(true);
@@ -94,7 +99,7 @@ var ViewModel = function(){
             animation: google.maps.Animation.DROP,
             id: i
           });
-          // Push the marker to our array of markers.
+          // Push the marker to our array of markers. 
           locations[i].marker = marker;
           // Create an onclick event to open an infowindow at each marker.
          // Push the marker to our array of markers.
@@ -105,6 +110,7 @@ var ViewModel = function(){
           });
         }
       }
+    
       function populateInfoWindow(marker) {
         // Check to make sure the infowindow is not already opened on this marker.
         if (infowindow.marker != marker) {
