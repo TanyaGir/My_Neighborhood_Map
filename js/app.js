7485e85374d2ff2c5ef35cd72c2e428cb4d6273c
William del Rosario <williamdelrosario@yahoo.com>
2017-09-28 16:11:05 +0200

    Zoom on clicked markers

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ var map;
 var marker;
 // Create a new blank array for all the listing markers.
 var markers = [];
-var locations = [{
+var locations = [ {
     name: 'Royal Palace Amsterdam',
     location: {
         lat: 52.373207,
@@ -34,158 +34,155 @@ var locations = [{
         lat: 52.3758,
         lng: 4.9092
     }
-},  {
+}, {
     name: 'University of Amsterdam',
     location: {
         lat: 52.3558,
         lng: 4.9557
     }
-}, {
-    name: 'Rijksmuseum Amsterdam',
-    location: {
-        lat: 52.2136,
-        lng: 4.5307
-    }
-}];
+} ];
 
-var Location = function(data) {
-    this.name = ko.observable(data.name);
-    this.visible = ko.observable(true);
-    this.location = ko.observable(data.location);
+var Location = function( data ) {
+    this.name = ko.observable( data.name );
+    this.visible = ko.observable( true );
+    this.location = ko.observable( data.location );
     this.marker = data.marker;
 };
 
 var ViewModel = function() {
     var self = this;
-    this.myLocations = ko.observableArray([]);
-    locations.forEach(function(locationItem) {
-        self.myLocations.push(new Location(locationItem));
-    });
-    this.currentlocation = ko.observable(this.myLocations()[0]);
-    this.markers = ko.observableArray(locations);
-    this.filter = ko.observable("");
-    this.wikipedia = ko.observableArray([]);
-    url = ko.observable(this.currentlocation);
-    details = ko.observable(url);
+    this.myLocations = ko.observableArray( [] );
+    locations.forEach( function( locationItem ) {
+        self.myLocations.push( new Location( locationItem ) );
+    } );
+    this.currentlocation = ko.observable( this.myLocations()[ 0 ] );
+    this.markers = ko.observableArray( locations );
+    this.filter = ko.observable( "" );
+    this.wikipedia = ko.observableArray( [] );
+    url = ko.observable( this.currentlocation );
+    details = ko.observable( url );
 
-  this.search = function(value) {
+    this.search = function( value ) {
         // remove all the current locations, which removes them from the view
-        if (value) {
+        if ( value ) {
             // filter the markers with the Marker setVisible method
-            for (var x in locations) {
-                if (locations[x].name.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
-                    self.myLocations()[x].visible(true);
-                    self.myLocations()[x].marker.setVisible(true);
+            for ( var x in locations ) {
+                if ( locations[ x ].name.toLowerCase().indexOf( value.toLowerCase() ) >= 0 ) {
+                    self.myLocations()[ x ].visible( true );
+                    self.myLocations()[ x ].marker.setVisible( true );
 
                 } else {
-                    self.myLocations()[x].visible(false);
-                    self.myLocations()[x].marker.setVisible(false);
+                    self.myLocations()[ x ].visible( false );
+                    self.myLocations()[ x ].marker.setVisible( false );
                 }
             }
         } else {
-            for (var y in locations) {
-                if (locations[y].name.toLowerCase().indexOf(value.toLowerCase()) >= 0)
-                    self.myLocations()[y].visible(true);
+            for ( var y in locations ) {
+                if ( locations[ y ].name.toLowerCase().indexOf( value.toLowerCase() ) >= 0 )
+                    self.myLocations()[ y ].visible( true );
             }
         }
     };
     this.initMap = function() {
         // Constructor creates a new map - only center and zoom are required.
-        map = new google.maps.Map(document.getElementById('map'), {
+        map = new google.maps.Map( document.getElementById( 'map' ), {
             center: {
                 lat: 52.371807,
                 lng: 4.896029
             },
             zoom: 13,
             mapTypeControl: false
-        });
+        } );
+
 
         infowindow = new google.maps.InfoWindow();
         // The following group uses the location array to create an array of markers on initialize.
-        for (var i = 0; i < locations.length; i++) {
+        for ( var i = 0; i < locations.length; i++ ) {
             // Get the position from the location array.
-            var position = locations[i].location;
-            var title = locations[i].name;
+            var position = locations[ i ].location;
+            var title = locations[ i ].name;
             // Create a marker per location, and put into markers array.
-            marker = new google.maps.Marker({
+            marker = new google.maps.Marker( {
                 map: map,
                 position: position,
                 title: title,
                 animation: google.maps.Animation.DROP,
                 id: i
-            });
-          // Push the marker to our array of markers.
-          markers.push(marker);
-          // Create an onclick event to open an infowindow at each marker.
-          marker.addListener('click', function() {
-            self.populateInfoWindow(this);
-          });
+            } );
+            // Push the marker to our array of markers.
+            markers.push( marker );
+            // Create an onclick event to open an infowindow at each marker.
+            marker.addListener( 'click', function() {
+                map.setZoom( 20 );
+                map.setCenter( marker.getPosition() );
+            } );
+
             // Push the marker to our array of markers. 
-            self.myLocations()[i].marker = marker;
+            self.myLocations()[ i ].marker = marker;
 
         }
     };
-    this.populateInfoWindow = function(marker) {
+    this.populateInfoWindow = function( marker ) {
         // Check to make sure the infowindow is not already opened on this marker.
-        if (infowindow.marker != marker) {
+        if ( infowindow.marker != marker ) {
             infowindow.marker = marker;
-            infowindow.setContent('<div>' + marker.title + '</div>');
-            infowindow.open(map, marker);
+            infowindow.setContent( '<div>' + marker.title + '</div>' );
+            infowindow.open( map, marker );
             // Make sure the marker property is cleared if the infowindow is closed.
-            infowindow.addListener('closeclick', function() {
+            infowindow.addListener( 'closeclick', function() {
                 infowindow.marker = null;
-                marker.addListener('click', function() {
-                    self.populateInfoWindow(this);
-                });
-            });
+                marker.addListener( 'click', function() {
+                    self.populateInfoWindow( this );
+                } );
+            } );
         }
     };
     //Click on item in list view
-    this.listViewClick = function(place) {
-        self.currentlocation(place);
-        self.populateInfoWindow(place.marker);
-        loadData(place);
-        if (place.name) {
-            place.marker.setAnimation(google.maps.Animation.BOUNCE); // Bounce marker when list view item is clicked
+    this.listViewClick = function( place ) {
+        self.currentlocation( place );
+        self.populateInfoWindow( place.marker );
+        loadData( place );
+        if ( place.name ) {
+            place.marker.setAnimation( google.maps.Animation.BOUNCE ); // Bounce marker when list view item is clicked
             //infowindow.open(map, place.marker); // Open info window on correct marker when list item is clicked
         }
-        setTimeout(function() {
-            place.marker.setAnimation(null); // End animation on marker after 2 seconds
-        }, 2000);
+        setTimeout( function() {
+            place.marker.setAnimation( null ); // End animation on marker after 2 seconds
+        }, 2000 );
     };
 };
 
-window.mapError = function (errorMsg, url, lineNumber) {
-    alert('Google Maps Failed To Load');
+window.mapError = function( errorMsg, url, lineNumber ) {
+    alert( 'Google Maps Failed To Load' );
 };
 
-var loadData = function (place) {
+var loadData = function( place ) {
 
     var wikiUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + place.name() + '&format=json&callback=wikiCallback';
-    
-    var wikiRequestTimeout = setTimeout(function() {
+
+    var wikiRequestTimeout = setTimeout( function() {
         // use a data binding instead of jQuery
         // http://knockoutjs.com/documentation/html-binding.html
-        alert("failed to get wikipedia resources");
-    }, 4000);
-    $.ajax({
+        alert( "failed to get wikipedia resources" );
+    }, 4000 );
+    $.ajax( {
         url: wikiUrl,
         dataType: "jsonp", // jsonp: "callback",
-        success: function(response) {
-            viewModel.wikipedia([]);
-            console.log(response);
-            var articleList = response[1];
-            for (var i = 0; i < articleList.length; i++) {
-                articleStr = articleList[i];
+        success: function( response ) {
+            viewModel.wikipedia( [] );
+            console.log( response );
+            var articleList = response[ 1 ];
+            for ( var i = 0; i < articleList.length; i++ ) {
+                articleStr = articleList[ i ];
                 var url = 'https://en.wikipedia.org/wiki/' + articleStr;
 
-                viewModel.wikipedia.push(url);
+                viewModel.wikipedia.push( url );
             }
-            clearTimeout(wikiRequestTimeout);
+            clearTimeout( wikiRequestTimeout );
         }
-    });
+    } );
 };
 
 var viewModel = new ViewModel();
-viewModel.filter.subscribe(viewModel.search);
-ko.applyBindings(viewModel);
+viewModel.filter.subscribe( viewModel.search );
+ko.applyBindings( viewModel );
